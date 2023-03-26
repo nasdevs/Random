c393663b1d4068ed9c030b94059d9dfdef9bf1f8
Nasrullah <89463001+nasdevs@users.noreply.github.com>
2023-03-26 19:00:31 +0800

    update

--- label-maker.py
+++ label-maker.py
@@ -8,23 +8,25 @@ from tqdm.auto import tqdm
 
 # contoh kode barang : 2022/III/S/ROU/02/{{ID}}/41
 
-kode_barang = str(input('Input kode barang : '))
+def main(kode_barang):
+    jumlah = int(kode_barang.split('/')[-1])
 
-jumlah = int(kode_barang.split('/')[-1])
+    kode_barang = kode_barang.replace('{', '', 1).replace('}', '', 1).replace(' ', '').split('/')
+    kode_barang = kode_barang[:-1] + ['0' * (3 - len(kode_barang[-1])) + kode_barang[-1]]
+    kode_barang = '/'.join(kode_barang)
 
-kode_barang = kode_barang.replace('{', '', 1).replace('}', '', 1).replace(' ', '').split('/')
-kode_barang = kode_barang[:-1] + ['0' * (3 - len(kode_barang[-1])) + kode_barang[-1]]
-kode_barang = '/'.join(kode_barang)
+    pag.hotkey('alt', 'tab')
 
-pag.hotkey('alt', 'tab')
+    print('Program running...')
+    time.sleep(1)
 
-print('Program running...')
-time.sleep(1)
+    for id in tqdm(range(1, jumlah+1)):
+        ID = '0' * (3 - len(str(id))) + str(id)
+        result = kode_barang.format(ID=ID)
+        pag.write(result)
+        pag.press('enter') 
 
-for id in tqdm(range(1, jumlah+1)):
-    ID = '0' * (3 - len(str(id))) + str(id)
-    result = kode_barang.format(ID=ID)
-    pag.write(result)
-    pag.press('enter') 
-
-print('DONE!')
+    print('DONE!')
+    
+ID = str(input('Input kode barang : '))
+main(ID)
